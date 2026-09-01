44a3c81a39ad1beb4d1a22b06de292c67772437f
김태정 <kimtejg@gmail.com>
2026-09-01 07:23:29 +0000

    자바스크립트 전체 작성

--- my_profile/script.js
+++ my_profile/script.js
@@ -1,1 +1,19 @@
 // 사용자와 상호작용하는 기능
+
+const aboutButton = document.getElementById("aboutButton");
+
+aboutButton.addEventListener("click", function () {
+
+    document
+        .getElementById("about")
+        .scrollIntoView({
+            behavior: "smooth"
+        });
+});
+
+const contactButton = document.getElementById("contactButton");
+
+contactButton.addEventListener("click", function () {
+
+    alert("방문해주셔서 감사합니다! :)");
+});
